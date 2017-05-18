28f0e84861caf27ef6751e62599997c0f727cfaa
umbum <umbum7601@gmail.com>
2017-05-18 19:13:09 +0900

    namechanger

--- namechanger/NameChanger.py
+++ namechanger/NameChanger.py
@@ -1,16 +1,21 @@
 #-*- coding: utf-8 -*-
 import os
 import sys
-import glob
+import re
 
 #파일(디렉토리) 이름으로 \ / : * ? " < > | 를 사용할 수 없다.
 
+dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 
-dir = input("target dir : ")
-
+po = re.compile(r"^[0-9]*\s+")
 try:
-    files = os.listdir(dir)
+    filelist = os.listdir(dir)
 except WindowsError:
     print("dir error")
     sys.exit()
+
+for f in filelist:
+    m = po.search(f)
+    if m:
+        os.rename(dir+ f, dir+ f[m.end():])
 
