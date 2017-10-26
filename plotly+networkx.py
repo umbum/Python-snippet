40d1d4a9bf9a5bd174ed159fb85a3a49479102b0
umbum <umbum7601@gmail.com>
2017-10-26 04:52:13 +0000

    terminate proj

--- plotly+networkx.py
+++ plotly+networkx.py
@@ -1,11 +1,12 @@
 #-*-coding:utf-8-*-
-from plotly.graph_objs import *
-from plotly.offline import plot
 from collections import OrderedDict
+from pprint import pprint
 
 import networkx as nx
+from plotly.graph_objs import *
+from plotly.offline import plot
 
-from pprint import pprint
+
 
 
 def build_graph():
@@ -29,10 +30,14 @@ def build_graph():
 
 #plotly		
 class Visualization:
-    def __init__(self, G):
-        self.G = G
-        self.traces = self._init_traces()
-        self.graph_to_traces()
+    def __init__(self):
+        self.G = dict()
+        self.traces = dict()
+        
+    def set_graph(self, G, key):
+        self.G[key] = G
+        self.traces[key] = self._init_traces()
+        self.graph_to_traces(key)
     
     def _init_traces(self):
         #앞에서 부터 그리기 때문에 OrderedDict.
@@ -56,55 +61,37 @@ class Visualization:
             text = [],
             mode = 'markers',
             marker = Marker(
-                size = 9,
+                size = [],
+                opacity=1,
                 colorscale='YlGnBu',
                 color=[],
-                line = dict(width=1.3)
+                line = {'width':1, 'color':'#555'}
                 )
             )
             
-        self.traces['root'] = Scatter(
-            name = 'root',
-            x = [],
-            y = [],
-            hoverinfo = 'text',
-            text = ['root'],
-            mode = 'markers',
-            marker = Marker(
-                size = 12,
-                color='#ff2a25',
-                line = dict(width=1)
-                )
-            )        
             
+        return traces
     
     def get_pos(self):
         return nx.circular_layout(self.G)
     '''
-    nx.nx_agraph.graphviz_layout(self.G, prog="twopi", root=1) : pygraphviz
-    nx.nx_pydot.graphviz_layout(self.G, prog="twopi", root=1)  : pydot 
+    nx.nx_agraph.graphviz_layout(self.G, prog="twopi", root=0) : pygraphviz
+    nx.nx_pydot.graphviz_layout(self.G, prog="twopi", root=0)  : pydot 
     nx의 graphviz_layout 메소드를 호출하면 자동으로 pygraphviz/pydot가 import되며 실행된다.
     근데 이거 꽤나 느리다. 호출하면 graphviz\\bin\\twopi.exe가 실행되며 pos값을 temp 파일에 쓰고, 
     python에서 temp 파일을 읽어와 반환하는 방식이다.
-    파일을 읽어와 반환하는 작업까지 한꺼번에 연결되어 있어 그냥은 최적화가 안된다.
-    데커레이터를 사용하면 가능할 것 같은데 안해봤다.
+    파일을 읽어와 반환하는 작업까지 한꺼번에 연결되어 있어 그냥은 최적화가 어렵다.
     '''
     
-    def graph_to_traces(self):
-        G = self.G
-        pos = self.get_pos()
+    def graph_to_traces(self.key):
+        G = self.G[key]
+        traces = self.traces[key]
+        pos = self.get_pos(key)
         
         #init root
         root_x = pos[0][0]
         root_y = pos[0][1]
-        self.traces['root']['x'].append(root_x)
-        self.traces['root']['y'].append(root_y)
-        text = ''
-        for key, value in G.node[0].items():
-            text += key + " : " + str(value) + "<br />"
-        self.traces['root']['text'].append(text)
-        del G.node[0]
-        
+
         for node, data in G.node.items():
             x, y = pos[node]
             if G.node[node]['status'] == 0:
@@ -120,6 +107,7 @@ class Visualization:
             self.traces[trace]['marker']['color'].append(((x - root_x)**2 + (y - root_y)**2)**0.5)
             # colorscale이 지정되어 있는 경우, 
             # color에 대충 값만 지정해도 알아서 값에 비례한 색상으로 바꿔준다.
+            self.traces[trace]['marker']['size'].append(9 + len(G.edge[node]))
 
         
         for edge in G.edges():
@@ -128,20 +116,52 @@ class Visualization:
             self.traces['edge']['x'] += [x0, x1, None]
             self.traces['edge']['y'] += [y0, y1, None]
             
+    
+    def table(self, fname):
+        tbl = [[]]
+        
+        ''' 처음부터 모든 리스트를 결합해 set으로 추려 n*m np.array를 할당하는
+        방식으로 진행하면 여기에 데이터를 채울 때 또 다시 반복을 돌아야 해서
+        2번 반복해야 한다.
+        이렇게 리스트를 붙여나가는 식으로 하면 한 번만 반복해도 됨.'''
+        for key in self.traces.keys():
+            tbl.append(['-' for x in range(len(tbl[0]))])
+            for node in self.G[key].node.values():
+                try:
+                    idx = tbl[0].index[node['filename']]
+                    tbl[-1][idx] = node['status']
+                except ValueError:
+                    tbl[0].append(node['filename'])
+                    tbl[-1].append(node['status'])
+                    
+        # transpose
+        tbl_t = list(zip(*tbl))
+        
+        # sort
+        tbl_t_s = sorted(tbl_t, key=lambda x : x[0])
+        
+        tbl_t_s.insert(0, ["node", *[col for col in self.traces.keys()]])
+        figure = ff.create_table(tbl_t_s)
+        plot(figure, filename=fname)
         
         
-    def visualize(self):
-        plot(Figure(data=Data(self.traces.values()),
+    def visualize(self, key, fname):
+        plot(Figure(data=Data(self.traces[key].values()),
         layout=Layout(
             hovermode='closest',
             xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
             yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)
             )
-        ), filename = "static")
+        ), filename = fname)
         
         
         
 if __name__ == "__main__":
+    v = Visualization()
+    
     G = build_graph()
-    v = Visualization(G)
-    v.visualize()
+    v.set_graph(G, 'graph1')
+    
+    v.visualize('graph1', 'graph1.html')
+    v.table('table.html')
+    
